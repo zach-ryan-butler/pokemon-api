91a3a551f56453eca1cb08926656aa18aab5a9c0
Zach Butler <zach.standley93@gmail.com>
2019-05-31 16:56:03 -0700

    built basic framework for master

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import Component from './Component.js';
+
+class App extends Component {
+
+    renderTemplate() {
+        return /*html*/`
+            <div>
+                <main>
+                </main>
+            </div>
+        `;
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import App from './Components/App.js';
+
+const root = document.getElementById('root');
+
+const app = new App();
+root.appendChild(app.render());
